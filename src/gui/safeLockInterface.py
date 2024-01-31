99b943ab69cb7dedc2a5f6cdbb266de394fdd860
ErikAndresFelea <erikandresfelea@gmail.com>
2024-01-31 21:33:45 +0100

    Feature: Password widget

--- src/gui/safeLockInterface.py
+++ src/gui/safeLockInterface.py
@@ -37,8 +37,29 @@ class App(customtkinter.CTk):
         # Show a list of items
         items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
         for i, item in enumerate(items):
-            label = customtkinter.CTkLabel(self, text=item)
-            label.grid(row=i, column=0, padx=20, pady=10)
+            item_widget = PasswordWidget(self)
+            item_widget.grid(row=i, column=0, padx=20, pady=10)
+
+
+class PasswordWidget(customtkinter.CTkFrame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.name_label = customtkinter.CTkLabel(self, text="Example_name")
+        self.name_label.grid(row=0, column=0, padx=20, pady=0)
+
+        self.password_label = customtkinter.CTkLabel(self, text="Example_password")
+        self.password_label.grid(row=2, column=0, padx=20, pady=0)
+
+        self.edit_button = customtkinter.CTkButton(self, text="Edit", width=50)
+        self.edit_button.grid(row=1, column=1, padx=5, pady=0)
+
+        self.view_button = customtkinter.CTkButton(self, text="View", width=50)
+        self.view_button.grid(row=1, column=2, padx=5, pady=0)
+
+        self.delete_button = customtkinter.CTkButton(self, text="Delete", width=50)
+        self.delete_button.grid(row=1, column=3, padx=5, pady=0)
+
 
 if __name__ == "__main__":
     app = App()
