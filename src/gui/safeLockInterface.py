53185d36fd2ae55c57fe1debe55a37577551701c
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-01 15:59:16 +0100

    Fix: Login_widget and safeLockInterface code

--- src/gui/safeLockInterface.py
+++ src/gui/safeLockInterface.py
@@ -1,5 +1,6 @@
 import customtkinter
 from widgets.Login_widget import LoginWidget
+from widgets.Password_widget import PasswordWidget
 
 
 customtkinter.set_appearance_mode("System")
@@ -21,7 +22,19 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure((0, 4), weight=1)
         self.grid_rowconfigure((1, 2, 3), weight=0)
 
-        login = LoginWidget(self)
+       # Login
+        self.login_widget = LoginWidget(self)
+        self.login_widget.grid(row=1, column=0, padx=20, pady=20)
+
+    def login(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        # Show a list of items
+        items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+        for i, item in enumerate(items):
+            item_widget = PasswordWidget(self)
+            item_widget.grid(row=i, column=0, padx=20, pady=10)
 
 
 if __name__ == "__main__":
--- src/gui/widgets/Login_widget.py
+++ src/gui/widgets/Login_widget.py
@@ -1,5 +1,4 @@
 import customtkinter
-from Password_widget import PasswordWidget
 
 class LoginWidget(customtkinter.CTkFrame):
     def __init__(self, master=None):
@@ -10,15 +9,5 @@ class LoginWidget(customtkinter.CTkFrame):
         self.password_entry = customtkinter.CTkEntry(self, placeholder_text="Contraseña", width=250)
         self.password_entry.grid(row=2, column=0, padx=20, pady=5)
         
-        self.button = customtkinter.CTkButton(self, text="Login", command=self.login)
-        self.button.grid(row=3, column=0, padx=20, pady=10)
-
-    def login(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-
-        # Show a list of items
-        items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
-        for i, item in enumerate(items):
-            item_widget = PasswordWidget(self)
-            item_widget.grid(row=i, column=0, padx=20, pady=10)
+        self.button = customtkinter.CTkButton(self, text="Login", command=master.login)
+        self.button.grid(row=3, column=0, padx=20, pady=5)
