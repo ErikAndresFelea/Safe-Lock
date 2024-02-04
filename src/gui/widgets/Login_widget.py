95b8bf5343f7debc53462ca6ce48ddbacace0bfe
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-04 10:52:25 +0100

    Login_widget: updated UI

--- src/gui/widgets/Login_widget.py
+++ src/gui/widgets/Login_widget.py
@@ -4,10 +4,13 @@ class LoginWidget(customtkinter.CTkFrame):
     def __init__(self, master=None):
         super().__init__(master)
 
+        self.logo_label = customtkinter.CTkLabel(self, text="Safe Lock", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=15)
+
         self.name_entry = customtkinter.CTkEntry(self, placeholder_text="Nombre", width=250)
         self.name_entry.grid(row=1, column=0, padx=20, pady=5)
         self.password_entry = customtkinter.CTkEntry(self, placeholder_text="Contraseña", width=250)
         self.password_entry.grid(row=2, column=0, padx=20, pady=5)
         
-        self.button = customtkinter.CTkButton(self, text="Login", command=master.login)
-        self.button.grid(row=3, column=0, padx=20, pady=5)
+        self.button = customtkinter.CTkButton(self, text="Login", command=master.login, width=75)
+        self.button.grid(row=3, column=0, padx=20, pady=(5, 15))
