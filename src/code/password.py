29be99edf9e8c48f117aa9227d121c1cbf1c4cd5
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-16 17:12:15 +0100

    Password - more methods

--- src/code/password.py
+++ src/code/password.py
@@ -7,7 +7,6 @@ class Password:
         self.app_id = app_id    # Posible id given by the app
         self.url = url
 
-
     # Getters
     def get_app_name(self) -> str:
         return self.app_name
@@ -26,20 +25,42 @@ class Password:
 
     def get_url(self) -> str:
         return self.url
-
+    
+    def get_all(self) -> list[str]:
+        return [self.unique_id, self.app_name, self.password, self.email, self.app_id, self.url]
 
     # Setters
-    def set_app_name(self, app_name):
+    def set_unique_id(self, unique_id: str):
+        self.unique_id = unique_id
+
+    def set_app_name(self, app_name: str):
         self.app_name = app_name
 
-    def set_password(self, password):
+    def set_password(self, password: str):
         self.password = password
 
-    def set_email(self, email):
+    def set_email(self, email: str):
         self.email = email
 
-    def set_app_id(self, app_id):
+    def set_app_id(self, app_id: str):
         self.app_id = app_id
 
-    def set_url(self, url):
+    def set_url(self, url: str):
         self.url = url
+
+    # The data has to be in the correct order and have 6 elements
+    def set_all(self, data: list[str]):
+        self.unique_id = data[0]
+        self.app_name = data[1]
+        self.password = data[2]
+        self.email = data[3]
+        self.app_id = data[4]
+        self.url = data[5]
+
+    def to_string(self):
+        print("Password:\nUnique id: " + self.unique_id 
+            + "\nName: " + self.app_name 
+            + "\nPassword: " + self.password 
+            + "\nEmail: " + self.email 
+            + "\nAPP ID: " + self.app_id 
+            + "\nURL: " + self.url )
