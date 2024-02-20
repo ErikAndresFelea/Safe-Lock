c3e46ec4fdd65206e9f3511cdc895c5f95f13c6e
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-20 20:25:05 +0100

    encrypt username for safety

--- src/code/login.py
+++ src/code/login.py
@@ -9,26 +9,34 @@ class Login:
         self.name = name
         self.password = password
         self.data_handler = DataHandler(None)
+        self.username = None
 
     # Check if password is correct
     def check_credentials(self) -> tuple[bool, DataHandler]:
         # Get encrypted key from windows password manager
         result, key = self.data_handler.obtain_key(self.name)
         if not result:
-            return False, None
+            return False, None, None
 
         # Decrypt password from file and check if its the same as the user input
         self.data_handler.set_key(key)
         with open(self.file, "r", encoding="utf-8") as json_file:
             data = json.load(json_file)
             users = data.get('users', {})
+            user_keys = users.keys()
 
-        if self.name not in users.keys():
-            return False, None
+        for user in user_keys:
+            current_user = self.data_handler.decrypt(user)
+            if (self.name == current_user):
+                self.username = user
+
+        # Check if user exists
+        if self.username is None:
+            return False, None, None
         
-        encrypted_password = users[self.name].get('app_password', '')
+        encrypted_password = users[self.username].get('app_password', '')
         confirm = self.authentiacte_user(encrypted_password, self.password)
-        return confirm, self.data_handler, self.name
+        return confirm, self.data_handler, self.username
 
 
     # Check if sotred password its the same as the user input
--- src/code/register.py
+++ src/code/register.py
@@ -27,6 +27,7 @@ class Register:
             return False
         
         # Encrypt the data
+        encrypted_username = data_hanlder.encrypt(self.user_name)
         encrypted_password = data_hanlder.encrypt(self.password)
         encrypted_email = data_hanlder.encrypt(self.email)
 
@@ -44,7 +45,7 @@ class Register:
             'email': encrypted_email,
             'all_passwords': []
             }
-        data['users'][self.user_name] = new_user
+        data['users'][encrypted_username] = new_user
 
         # Save the data into the storage file 
         with open(self.storage_file, "w", encoding="utf-8") as json_file:
