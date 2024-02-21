4e3e9a9862628cff71cf600f91912b50af46d30f
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-21 15:24:45 +0100

    add_pass_widget - double check input password

--- src/gui/widgets/add_pass_widget.py
+++ src/gui/widgets/add_pass_widget.py
@@ -11,7 +11,7 @@ class AddPasswordWidget(customtkinter.CTkFrame):
         # Top frame. 2 columns and 5 rows
         self.top_frame = customtkinter.CTkFrame(self)
         self.top_frame.grid(row=0, column=0, padx=0, pady=0)
-        self.top_frame.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+        self.top_frame.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
         self.top_frame.grid_columnconfigure((0, 1), weight=1)
 
         self.name_label = customtkinter.CTkLabel(self.top_frame, text="Nombre")
@@ -24,20 +24,25 @@ class AddPasswordWidget(customtkinter.CTkFrame):
         self.password_entry = customtkinter.CTkEntry(self.top_frame, placeholder_text="******", width=250)
         self.password_entry.grid(row=1, column=1, padx=20, pady=5)
 
+        self.confirm_password_label = customtkinter.CTkLabel(self.top_frame, text="Confirmar contraseña")
+        self.confirm_password_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+        self.repeat_password_entry = customtkinter.CTkEntry(self.top_frame, placeholder_text="******", width=250)
+        self.repeat_password_entry.grid(row=2, column=1, padx=20, pady=5)
+
         self.email_label = customtkinter.CTkLabel(self.top_frame, text="Email")
-        self.email_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+        self.email_label.grid(row=3, column=0, padx=20, pady=5, sticky="w")
         self.email_entry = customtkinter.CTkEntry(self.top_frame, placeholder_text="ejemplo@mail.com", width=250)
-        self.email_entry.grid(row=2, column=1, padx=20, pady=5)
+        self.email_entry.grid(row=3, column=1, padx=20, pady=5)
 
         self.app_id_label = customtkinter.CTkLabel(self.top_frame, text="ID")
-        self.app_id_label.grid(row=3, column=0, padx=20, pady=5, sticky="w")
+        self.app_id_label.grid(row=4, column=0, padx=20, pady=5, sticky="w")
         self.app_id_entry = customtkinter.CTkEntry(self.top_frame, placeholder_text="Identificador app", width=250)
-        self.app_id_entry.grid(row=3, column=1, padx=20, pady=5)
+        self.app_id_entry.grid(row=4, column=1, padx=20, pady=5)
 
         self.url_label = customtkinter.CTkLabel(self.top_frame, text="URL")
-        self.url_label.grid(row=4, column=0, padx=20, pady=5, sticky="w")
+        self.url_label.grid(row=5, column=0, padx=20, pady=5, sticky="w")
         self.url_entry = customtkinter.CTkEntry(self.top_frame, placeholder_text="link a pagina web", width=250)
-        self.url_entry.grid(row=4, column=1, padx=20, pady=5)
+        self.url_entry.grid(row=5, column=1, padx=20, pady=5)
 
         # Bottom frame. 2 columns and 1 row
         self.bottom_frame = customtkinter.CTkFrame(self)
@@ -51,5 +56,9 @@ class AddPasswordWidget(customtkinter.CTkFrame):
         self.cancel_button.grid(row=0, column=1, padx=5, pady=(5, 15), sticky="w")
 
     def add_pass(self):
-        password_data = ["", self.name_entry.get().capitalize(), self.password_entry.get(), self.email_entry.get(), self.app_id_entry.get(), self.url_entry.get()]
-        self.parent_app.add_pass(password_data)
+        if self.password_entry.get() == self.repeat_password_entry.get():
+            password_data = ["", self.name_entry.get().capitalize(), self.password_entry.get(), self.repeat_password_entry.get(), self.email_entry.get(), self.app_id_entry.get(), self.url_entry.get()]
+            self.parent_app.add_pass(password_data)
+        else:
+            '''Ui error message'''
+            pass
