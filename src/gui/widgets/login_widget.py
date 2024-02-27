58c970eb949bb69b33ea9d978db51222aa05cfdc
ErikAndresFelea <erikandresfelea@gmail.com>
2024-02-27 15:11:19 +0100

    improved code

--- src/gui/widgets/login_widget.py
+++ src/gui/widgets/login_widget.py
@@ -27,8 +27,8 @@ class LoginWidget(ctk.CTkFrame):
         login_frame.grid(row=1, column=0, padx=20, pady=20)
         login_frame.grid_rowconfigure((0, 1, 2), weight=1)
         
-        self.user_label = ctk.CTkLabel(login_frame, text=None, text_color="red", font=ctk.CTkFont(size=10))
-        self.user_label.grid(row=0, column=0, padx=20, pady=20)
+        error_label = ctk.CTkLabel(login_frame, text=None, text_color="red", font=ctk.CTkFont(size=10))
+        error_label.grid(row=0, column=0, padx=20, pady=20)
         
         self.user_entry = ctk.CTkEntry(login_frame, placeholder_text="Usuario *", width=250)
         self.user_entry.grid(row=1, column=0, padx=20, pady=20)
@@ -40,17 +40,17 @@ class LoginWidget(ctk.CTkFrame):
         check_forgot_frame.grid(row=2, column=0, padx=20, pady=20)
         check_forgot_frame.grid_columnconfigure((0, 1), weight=1)
 
-        self.remember_checkbox = ctk.CTkCheckBox(check_forgot_frame, text="Recordar", checkbox_width=18, checkbox_height=18, border_width=2)
-        self.remember_checkbox.grid(row=0, column=0, padx=20, pady=20)
+        remember_checkbox = ctk.CTkCheckBox(check_forgot_frame, text="Recordar", checkbox_width=18, checkbox_height=18, border_width=2)
+        remember_checkbox.grid(row=0, column=0, padx=20, pady=20)
 
-        self.forgot_button = ctk.CTkButton(check_forgot_frame, text="¿Contraseña olvidada?", text_color="deepskyblue", command=app.view_forgot_pass, border_width=0, corner_radius=0, bg_color="transparent", fg_color="transparent", hover=False)
-        self.forgot_button.grid(row=0, column=1, padx=20, pady=20)
+        forgot_button = ctk.CTkButton(check_forgot_frame, text="¿Contraseña olvidada?", text_color="deepskyblue", command=app.view_forgot_pass, border_width=0, corner_radius=0, bg_color="transparent", fg_color="transparent", hover=False)
+        forgot_button.grid(row=0, column=1, padx=20, pady=20)
 
-        self.login_button = ctk.CTkButton(self, text="Iniciar sesion", command=self.login, width=75)
-        self.login_button.grid(row=3, column=0, padx=20, pady=20)
+        login_button = ctk.CTkButton(self, text="Iniciar sesion", command=self.login, width=75)
+        login_button.grid(row=3, column=0, padx=20, pady=20)
         
-        self.register_button = ctk.CTkButton(self, text="Registrarse", text_color="deepskyblue", command=app.view_register, border_width=0, corner_radius=0, bg_color="transparent", fg_color="transparent", hover=False)
-        self.register_button.grid(row=4, column=0, padx=20, pady=20)
+        register_button = ctk.CTkButton(self, text="Registrarse", text_color="deepskyblue", command=app.view_register, border_width=0, corner_radius=0, bg_color="transparent", fg_color="transparent", hover=False)
+        register_button.grid(row=4, column=0, padx=20, pady=20)
         
 
     ''' 
@@ -66,7 +66,7 @@ class LoginWidget(ctk.CTkFrame):
             if error:
                 print("Error a la hora de realizar el login: " + data)
             elif not status:
-                self.user_label.configure(text="Credenciales invalidas")
+                self.error_label.configure(text="Credenciales invalidas")
                 self.user_entry.configure(border_color="darkred")
                 self.password_entry.configure(border_color="darkred")
             else:
@@ -78,22 +78,22 @@ class LoginWidget(ctk.CTkFrame):
         password = len(self.password_entry.get()) > 0
 
         if (not user and not password):
-            self.user_label.configure(text="Introduce credenciales")
+            self.error_label.configure(text="Introduce credenciales")
             self.user_entry.configure(border_color="darkred")
             self.password_entry.configure(border_color="darkred")
 
         elif not user:
-            self.user_label.configure(text="Introduce usuario")
+            self.error_label.configure(text="Introduce usuario")
             self.user_entry.configure(border_color="darkred")
 
         elif not password:
-            self.user_label.configure(text="Introduce contraseña")
+            self.error_label.configure(text="Introduce contraseña")
             self.password_entry.configure(border_color="darkred")
         return user and password
     
 
     def reset_ui(self):
-        self.user_label.configure(text=None)
+        self.error_label.configure(text=None)
         self.user_entry.configure(border_color="gray50")
         self.password_entry.configure(border_color="gray50")
         
--- src/gui/widgets/register_widget.py
+++ src/gui/widgets/register_widget.py
@@ -12,42 +12,45 @@ class RegisterWidget(ctk.CTkFrame):
         title_label.grid(row=0, column=0, padx=20, pady=20)
 
         # Top frame. 2 columns and 5 rows
-        self.top_frame = ctk.CTkFrame(self)
-        self.top_frame.grid(row=1, column=0, padx=20, pady=20)
-        self.top_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
-        self.top_frame.grid_columnconfigure((0, 1), weight=1)
+        form_frame = ctk.CTkFrame(self)
+        form_frame.grid(row=1, column=0, padx=20, pady=20)
+        form_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        form_frame.grid_columnconfigure((0, 1), weight=1)
 
-        self.name_label = ctk.CTkLabel(self.top_frame, text="Usuario")
-        self.name_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
-        self.name_entry = ctk.CTkEntry(self.top_frame, placeholder_text="Nombre de usuario *", width=250)
-        self.name_entry.grid(row=0, column=1, padx=20, pady=20)
+        # error_label = ctk.CTkLabel(form_frame, text=None, text_color="red", font=ctk.CTkFont(size=10))
+        # error_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.email_label = ctk.CTkLabel(self.top_frame, text="Email")
-        self.email_label.grid(row=1, column=0, padx=20, pady=20, sticky="w")
-        self.email_entry = ctk.CTkEntry(self.top_frame, placeholder_text="ejemplo@gmail.com *", width=250)
+        user_label = ctk.CTkLabel(form_frame, text="Usuario")
+        user_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
+        self.user_entry = ctk.CTkEntry(form_frame, placeholder_text="Nombre de usuario *", width=250)
+        self.user_entry.grid(row=0, column=1, padx=20, pady=20)
+
+        email_label = ctk.CTkLabel(form_frame, text="Email")
+        email_label.grid(row=1, column=0, padx=20, pady=20, sticky="w")
+        self.email_entry = ctk.CTkEntry(form_frame, placeholder_text="ejemplo@gmail.com *", width=250)
         self.email_entry.grid(row=1, column=1, padx=20, pady=20)
 
-        self.password_label = ctk.CTkLabel(self.top_frame, text="Contraseña")
-        self.password_label.grid(row=2, column=0, padx=20, pady=20, sticky="w")
-        self.password_entry = ctk.CTkEntry(self.top_frame, placeholder_text="******", width=250)
+        password_label = ctk.CTkLabel(form_frame, text="Contraseña")
+        password_label.grid(row=2, column=0, padx=20, pady=20, sticky="w")
+        self.password_entry = ctk.CTkEntry(form_frame, placeholder_text="******", width=250)
         self.password_entry.grid(row=2, column=1, padx=20, pady=20)
 
-        self.conf_password_label = ctk.CTkLabel(self.top_frame, text="Repetir contraseña")
-        self.conf_password_label.grid(row=3, column=0, padx=20, pady=20, sticky="w")
-        self.conf_password_entry = ctk.CTkEntry(self.top_frame, placeholder_text="******", width=250)
-        self.conf_password_entry.grid(row=3, column=1, padx=20, pady=20)
+        rep_password_label = ctk.CTkLabel(form_frame, text="Repetir contraseña")
+        rep_password_label.grid(row=3, column=0, padx=20, pady=20, sticky="w")
+        self.rep_password_entry = ctk.CTkEntry(form_frame, placeholder_text="******", width=250)
+        self.rep_password_entry.grid(row=3, column=1, padx=20, pady=20)
 
 
         # Bottom frame. 2 columns and 1 row
-        self.bottom_frame = ctk.CTkFrame(self)
-        self.bottom_frame.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
-        self.bottom_frame.grid_columnconfigure((0, 1), weight=1)
+        button_frame = ctk.CTkFrame(self)
+        button_frame.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
+        button_frame.grid_columnconfigure((0, 1), weight=1)
 
-        self.cancel_button = ctk.CTkButton(self.bottom_frame, text="Cancelar", command=app.welcome_screen, width=75)
-        self.cancel_button.grid(row=0, column=0, padx=20, pady=20, sticky="e")
+        cancel_button = ctk.CTkButton(button_frame, text="Cancelar", command=app.welcome_screen, width=75)
+        cancel_button.grid(row=0, column=0, padx=20, pady=20, sticky="e")
 
-        self.register_button = ctk.CTkButton(self.bottom_frame, text="Registrarse", command=self.register, width=75)
-        self.register_button.grid(row=0, column=1, padx=20, pady=20, sticky="w")
+        register_button = ctk.CTkButton(button_frame, text="Registrarse", command=self.register, width=75)
+        register_button.grid(row=0, column=1, padx=20, pady=20, sticky="w")
 
 
     def register(self):
@@ -55,7 +58,7 @@ class RegisterWidget(ctk.CTkFrame):
         status = self.check_user_input()
 
         if status:
-            error, status, data = self.parent_app.register(self.name_entry.get(), self.email_entry.get(), self.password_entry.get(), self.conf_password_entry.get())
+            error, status, data = self.parent_app.register(self.user_entry.get(), self.email_entry.get(), self.password_entry.get(), self.rep_password_entry.get())
             if error:
                 print("Error a la hora de realizar el registro: " + data)
             elif not status:
@@ -67,14 +70,14 @@ class RegisterWidget(ctk.CTkFrame):
 
 
     def check_user_input(self) -> bool:
-        user = len(self.name_entry.get()) > 0
+        user = len(self.user_entry.get()) > 0
         email = len(self.email_entry.get()) > 0
         password = len(self.password_entry.get()) > 0
-        rep_password = len(self.conf_password_entry.get()) > 0
+        rep_password = len(self.rep_password_entry.get()) > 0
 
         if not user:
             # self.user_label.configure(text="Introduce ..."
-            self.name_entry.configure(border_color="darkred")
+            self.user_entry.configure(border_color="darkred")
 
         if not email:
             # self.user_label.configure(text="Introduce ..."
@@ -86,19 +89,19 @@ class RegisterWidget(ctk.CTkFrame):
 
         if not rep_password:
             # self.user_label.configure(text="Introduce ..."
-            self.conf_password_entry.configure(border_color="darkred")
+            self.rep_password_entry.configure(border_color="darkred")
 
-        if not self.password_entry.get() == self.conf_password_entry.get():
+        if not self.password_entry.get() == self.rep_password_entry.get():
             # self.user_label.configure(text="Contraseñas no coinciden")
             self.password_entry.configure(border_color="darkred")
-            self.conf_password_entry.configure(border_color="darkred")
+            self.rep_password_entry.configure(border_color="darkred")
             return False
         return user and email and password and rep_password
 
 
     def reset_ui(self):
         # self.user_label.configure(text=None)
-        self.name_entry.configure(border_color="gray50")
+        self.user_entry.configure(border_color="gray50")
         self.email_entry.configure(border_color="gray50")
         self.password_entry.configure(border_color="gray50")
-        self.conf_password_entry.configure(border_color="gray50")
+        self.rep_password_entry.configure(border_color="gray50")
