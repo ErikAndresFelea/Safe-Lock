a2a979558655df0e5993da40c182645985867d68
ErikAndresFelea <erikandresfelea@gmail.com>
2024-03-20 02:10:24 +0100

    Update UI

--- src/gui/widgets/home_widget.py
+++ src/gui/widgets/home_widget.py
@@ -1,25 +1,34 @@
 import customtkinter as ctk
+import tkinter as tk
 from gui.widgets.password_widget import PasswordWidget
 
 class HomeWidget(ctk.CTkFrame):
     def __init__(self, master, app):
-        super().__init__(master)
+        super().__init__(master, fg_color="transparent")
         self.parent_app = app
         self.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
         title_label = ctk.CTkLabel(self, text="Contraseñas", font=ctk.CTkFont(size=40, weight="bold", family="Verdana"))
         title_label.grid(row=0, column=0, padx=20, pady=20)
         
-        self.error_label = ctk.CTkLabel(self, text=None, text_color="red", font=ctk.CTkFont(size=10))
-        self.error_label.grid(row=1, column=0, padx=20, pady=20)
+        self.error_label = ctk.CTkLabel(self, text=None, text_color="red", font=ctk.CTkFont(size=12))
+        self.error_label.grid(row=0, column=1, padx=8, pady=0, sticky="w")
 
         all_passwords = self.get_passwords()
-        main_frame = ctk.CTkScrollableFrame(self, width=600)
-        main_frame.grid(row=2, column=0, padx=20, pady=20)
+        main_frame = ctk.CTkScrollableFrame(self, width=600, fg_color="transparent")
+        main_frame.grid(row=2, column=0, padx=0, pady=0)
+        main_frame.grid_rowconfigure(tuple(range(len(all_passwords))), weight=1)
+        main_frame.grid_columnconfigure(0, weight=1)
 
-        for i in range(len(all_passwords)):
-            password_frame = PasswordWidget(main_frame, app, all_passwords[i]) # for some reason not displayed
-            password_frame.grid(row=i, column=0, padx=20, pady=20)
+        counter = 0
+        for i in range(0, len(all_passwords) * 2, 2):
+            password_frame = PasswordWidget(main_frame, app, all_passwords[counter])
+            password_frame.grid(row=i, column=0, padx=0, pady=0, sticky="ew")
+            counter += 1
+
+            if counter != len(all_passwords):
+                frame = ctk.CTkFrame(main_frame, height=4, border_width=1, fg_color="gray50")
+                frame.grid(row=i+1, column=0, padx=5, pady=5, sticky="we")
 
         add_button = ctk.CTkButton(self, text="Añadir", command=app.view_add_pass, width=75)
         add_button.grid(row=3, column=0, padx=20, pady=20)
--- src/gui/widgets/password_widget.py
+++ src/gui/widgets/password_widget.py
@@ -15,18 +15,14 @@ class PasswordWidget(ctk.CTkFrame):
         super().__init__(master, fg_color="transparent")
         self.parent_app = app
         self.password_data = data
-
+        
         self.grid_columnconfigure((0, 1), weight=1)
 
-        # Widget split into 2 frames. Left and Right
-        main_frame = ctk.CTkFrame(self, fg_color="transparent")
-        main_frame.grid(row=0, column=0, padx=0, pady=0)
-        main_frame.grid_columnconfigure((0, 1), weight=1)
-
         # Left frame. 1 column and 2 rows
-        left_frame = ctk.CTkFrame(main_frame, fg_color="transparent")
-        left_frame.grid(row=0, column=0, padx=(0, 10), pady=0)
+        left_frame = ctk.CTkFrame(self, fg_color="transparent")
+        left_frame.grid(row=0, column=0, padx=(0, 10), pady=0, sticky="nsw")
         left_frame.grid_rowconfigure((0, 1), weight=1)
+        left_frame.grid_columnconfigure(0, weight=1)
         
         name_label = ctk.CTkLabel(left_frame, text=data[1], cursor="hand2")
         name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
@@ -37,19 +33,19 @@ class PasswordWidget(ctk.CTkFrame):
         self.password_label.bind("<Button-1>", self.on_click)
 
         # Right frame. 3 columns and 1 row
-        right_frame = ctk.CTkFrame(main_frame, fg_color="transparent")
-        right_frame.grid(row=0, column=1, padx=(10, 0), pady=0)
+        right_frame = ctk.CTkFrame(self, fg_color="transparent")
+        right_frame.grid(row=0, column=1, padx=(10, 0), pady=0, sticky="nse")
         right_frame.grid_rowconfigure(0, weight=1)
         right_frame.grid_columnconfigure((0, 1, 2), weight=1)
 
         view_button = ctk.CTkButton(right_frame, text="View", command=self.view_pass, width=50)
-        view_button.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        view_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
         edit_button = ctk.CTkButton(right_frame, text="Edit", command=self.view_update_pass, width=50)
-        edit_button.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
+        edit_button.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
 
         delete_button = ctk.CTkButton(right_frame, text="Delete", command=self.delete_pass, width=50)
-        delete_button.grid(row=0, column=3, padx=5, pady=5, sticky="ew")
+        delete_button.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
 
 
     ''' Button for app info display '''
