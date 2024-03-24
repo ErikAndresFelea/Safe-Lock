d12ce05cb9b1d7696a44807f63eb964e92a70a8b
ErikAndresFelea <erikandresfelea@gmail.com>
2024-03-24 06:34:47 +0100

    Updated UI

--- src/gui/widgets/add_pass_widget.py
+++ src/gui/widgets/add_pass_widget.py
@@ -3,9 +3,10 @@ import customtkinter as ctk
 ''' 
 The interface is divided in 3 rows. Each row contains:
     · 1st: Title label
-    · 2nd: A frame with 7 rows:
+    · 2nd: A frame with 8 rows:
         · Label for feedback msg
         · Name label & entry
+        · Username label & entry
         · Password label & entry
         · Rep. password label & entry
         · Email label & entry
@@ -29,7 +30,7 @@ class AddPasswordWidget(ctk.CTkFrame):
         # Top frame. 2 columns and 6 rows
         form_frame = ctk.CTkFrame(self, fg_color="transparent")
         form_frame.grid(row=1, column=0, padx=0, pady=0)
-        form_frame.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+        form_frame.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=1)
         form_frame.grid_columnconfigure((0, 1), weight=1)
 
         self.error_label = ctk.CTkLabel(form_frame, text=None, text_color="red", font=ctk.CTkFont(size=12))
@@ -37,33 +38,38 @@ class AddPasswordWidget(ctk.CTkFrame):
 
         name_label = ctk.CTkLabel(form_frame, text="Nombre:")
         name_label.grid(row=1, column=0, padx=5, pady=(0, 5), sticky="w")
-        self.name_entry = ctk.CTkEntry(form_frame, placeholder_text="Nombre", width=250)
+        self.name_entry = ctk.CTkEntry(form_frame, placeholder_text="Aplicación", width=250)
         self.name_entry.grid(row=1, column=1, padx=5, pady=(0, 5))
 
+        user_label = ctk.CTkLabel(form_frame, text="Usuario:")
+        user_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        self.user_entry = ctk.CTkEntry(form_frame, placeholder_text="Usuario", width=250)
+        self.user_entry.grid(row=2, column=1, padx=5, pady=5)
+
         password_label = ctk.CTkLabel(form_frame, text="Contraseña:")
-        password_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        password_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
         self.password_entry = ctk.CTkEntry(form_frame, placeholder_text="******", show="*", width=250)
-        self.password_entry.grid(row=2, column=1, padx=5, pady=5)
+        self.password_entry.grid(row=3, column=1, padx=5, pady=5)
 
         rep_password_label = ctk.CTkLabel(form_frame, text="Rep. contraseña:")
-        rep_password_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+        rep_password_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
         self.rep_password_entry = ctk.CTkEntry(form_frame, placeholder_text="******", show="*", width=250)
-        self.rep_password_entry.grid(row=3, column=1, padx=5, pady=5)
+        self.rep_password_entry.grid(row=4, column=1, padx=5, pady=5)
 
         email_label = ctk.CTkLabel(form_frame, text="Email:")
-        email_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
+        email_label.grid(row=5, column=0, padx=5, pady=5, sticky="w")
         self.email_entry = ctk.CTkEntry(form_frame, placeholder_text="(Opcional)", width=250)
-        self.email_entry.grid(row=4, column=1, padx=5, pady=5)
+        self.email_entry.grid(row=5, column=1, padx=5, pady=5)
 
         app_id_label = ctk.CTkLabel(form_frame, text="ID:")
-        app_id_label.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+        app_id_label.grid(row=6, column=0, padx=5, pady=5, sticky="w")
         self.app_id_entry = ctk.CTkEntry(form_frame, placeholder_text="(Opcional)", width=250)
-        self.app_id_entry.grid(row=5, column=1, padx=5, pady=5)
+        self.app_id_entry.grid(row=6, column=1, padx=5, pady=5)
 
         url_label = ctk.CTkLabel(form_frame, text="URL:")
-        url_label.grid(row=6, column=0, padx=5, pady=5, sticky="w")
+        url_label.grid(row=7, column=0, padx=5, pady=5, sticky="w")
         self.url_entry = ctk.CTkEntry(form_frame, placeholder_text="(Opcional)", width=250)
-        self.url_entry.grid(row=6, column=1, padx=5, pady=5)
+        self.url_entry.grid(row=7, column=1, padx=5, pady=5)
 
         # Bottom frame. 2 columns and 1 row
         button_frame = ctk.CTkFrame(self, fg_color="transparent")
@@ -86,7 +92,7 @@ class AddPasswordWidget(ctk.CTkFrame):
         user_input_validation = self.check_user_input()
         
         if user_input_validation:
-            error, status, data  = self.parent_app.add_pass(["", self.name_entry.get().capitalize(), self.password_entry.get(), self.email_entry.get(), self.app_id_entry.get(), self.url_entry.get()])
+            error, status, data  = self.parent_app.add_pass(["", self.name_entry.get().capitalize(), self.user_entry.get(), self.password_entry.get(), self.email_entry.get(), self.app_id_entry.get(), self.url_entry.get()])
             if error:
                 print("Error al añadir la contraseña: " + data)
             elif not status:
@@ -98,15 +104,18 @@ class AddPasswordWidget(ctk.CTkFrame):
     ''' User input validation '''
     def check_user_input(self) -> bool:
         name = len(self.name_entry.get()) > 0
+        user = len(self.user_entry.get()) > 0
         password = len(self.password_entry.get()) > 0
         rep_password = len(self.rep_password_entry.get()) > 0
         both_password = (self.password_entry.get() == self.rep_password_entry.get())
 
         # Update UI with msg & color feedback
-        if not (name and password and rep_password):
+        if not (name and user and password and rep_password):
             self.error_label.configure(text="Verifica los campos marcados") 
             if not name:
                 self.name_entry.configure(border_color="darkred")
+            if not user:
+                self.user_entry.configure(border_color="darkred")
             if not password:
                 self.password_entry.configure(border_color="darkred")
             if not rep_password:
@@ -117,13 +126,14 @@ class AddPasswordWidget(ctk.CTkFrame):
             self.error_label.configure(text="Las contraseñas no coinciden")
             self.password_entry.configure(border_color="darkred")
             self.rep_password_entry.configure(border_color="darkred")
-        return name and password and rep_password and both_password
+        return name and user and password and rep_password and both_password
     
 
     ''' Sets UI dynamic elements to default '''
     def reset_ui(self):
         self.error_label.configure(text=None)
         self.name_entry.configure(border_color="gray50")
+        self.user_entry.configure(border_color="gray50")
         self.password_entry.configure(border_color="gray50")
         self.rep_password_entry.configure(border_color="gray50")
         
