0078cbd787d330817d8764b24f192abab4ce6597
ErikAndresFelea <erikandresfelea@gmail.com>
2024-03-25 18:06:51 +0100

    fix

--- src/gui/widgets/password_widget.py
+++ src/gui/widgets/password_widget.py
@@ -12,30 +12,30 @@ The interface is divided in 2 columns. Each column contains:
 '''
 class PasswordWidget(ctk.CTkFrame):
     def __init__(self, master, app, data: list[str]):
-        super().__init__(master, fg_color="transparent")
+        super().__init__(master, fg_color="gray17", border_width=2)
         self.parent_app = app
         self.password_data = data
         
         self.grid_columnconfigure((0, 1), weight=1)
 
         # Left frame. 1 column and 2 rows
-        left_frame = ctk.CTkFrame(self, fg_color="transparent")
-        left_frame.grid(row=0, column=0, padx=(0, 10), pady=0, sticky="nsw")
+        left_frame = ctk.CTkFrame(self, fg_color="gray17")
+        left_frame.grid(row=0, column=0, padx=(3, 10), pady=3, sticky="nsw")
         left_frame.grid_rowconfigure((0, 1), weight=1)
         left_frame.grid_columnconfigure(0, weight=1)
         
-        name_label = ctk.CTkLabel(left_frame, text=data[1], cursor="hand2")
-        name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        name_label.bind("<Button-1>", self.on_click)
+        user_name_label = ctk.CTkLabel(left_frame, text=data[2], cursor="hand2")
+        user_name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        user_name_label.bind("<Button-1>", self.on_click)
 
         #password_label_text = "*" * len(data[2])
-        self.password_label = ctk.CTkLabel(left_frame, text=data[2], cursor="hand2")
+        self.password_label = ctk.CTkLabel(left_frame, text=data[3], cursor="hand2")
         self.password_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
         self.password_label.bind("<Button-1>", self.on_click)
 
         # Right frame. 3 columns and 1 row
-        right_frame = ctk.CTkFrame(self, fg_color="transparent")
-        right_frame.grid(row=0, column=1, padx=(10, 0), pady=0, sticky="nse")
+        right_frame = ctk.CTkFrame(self, fg_color="gray17")
+        right_frame.grid(row=0, column=1, padx=(10, 3), pady=3, sticky="nse")
         right_frame.grid_rowconfigure(0, weight=1)
         right_frame.grid_columnconfigure((0, 1, 2), weight=1)
 
