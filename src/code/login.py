e2c94d9686e2bf3c655d9e286cf096b695b05e87
Erik Andres F.H. <erikandresfelea@gmail.com>
2024-07-02 20:41:50 +0200

    updated:   src/code/login.py

--- src/code/login.py
+++ src/code/login.py
@@ -1,50 +1,41 @@
-import json
+import sqlite3 as sql
 from code.dataHandler import DataHandler
 
-Operation = bool
-Error = bool
-Msg = str | None
 
 class Login:
-    def __init__(self, user: str, password: str, data_hanlder: DataHandler, remember: bool) -> None:
-        self.user = user
-        self.password = password
-        self.data_handler = data_hanlder
-        self.remember = remember
-
-
-    ''' 
-    Checks if there is a user registered with the password provided
-    '''
-    def check_credentials(self) -> tuple[Error, Operation, DataHandler | None, Msg]:
-        # Checks if the user has a key
-        error, status, data = self.data_handler.obtain_key(self.user)
-        if not status:
-            return error, status, None, data
-
-        # Checks if user is registered
-        error, status, json_data = self.data_handler.json_load()
-        if not status or error:
-                return error, status, json_data
-        users = json_data.get('users', {})
-
-        # Checks if user password is correct
-        self.data_handler.set_key(data)
-        encrypted_password = users[self.user].get('app_password', '')
-        error, status, data = self.authentiacte_password(encrypted_password, self.password)
-        if not status:
-            return error, status, None, data
+    def __init__(self, connection: sql.Connection, username: str, password: str, remember: bool) -> None:
+        self._connection = connection
+        self.user_id = None
+        self.data_handler = None
+        self.authenticated = False
         
-        json_data['remember'] = (self.user if self.remember else "")
-        error, status, json_data = self.data_handler.json_dump(json_data)
-        if not status or error:
-                return error, status, json_data
-        return False, True, self.data_handler, self.user
+        self._authenticate_user(username, password)
+        if self.authenticated:
+            self._set_remember(remember)
 
 
-    # Obtains user stored password and checks if its same as provided
-    def authentiacte_password(self, encrypted_password: str, user_password: str) -> tuple[Error, Operation, Msg]:
-        error, status, data = self.data_handler.decrypt(encrypted_password)
-        if error:
-            return True, status, data
-        return False, user_password == data, None
+    def _authenticate_user(self, username: str, password: str) -> None:
+        cursor = self._connection.cursor()
+        cursor.execute(f'''SELECT id, password, encryption_key FROM users WHERE username == '{username}';''')
+        user_data = cursor.fetchone()
+
+        if len(user_data) != 0:
+            self.data_handler = DataHandler(user_data[2])
+            operation, decrypted_password = self.data_handler.decrypt(user_data[1])
+
+            if operation and decrypted_password == password:
+                self.user_id = user_data[0]
+                self.authenticated = True
+
+        cursor.close()
+    
+
+    def _set_remember(self, remember: bool) -> None:
+        cursor = self._connection.cursor()
+        cursor.execute('''UPDATE TABLE users SET remember_login = FALSE''')
+        
+        if remember:
+            cursor.execute(f'''UPDATE TABLE users SET remember_login = TRUE WHERE id == '{self.user_id}';''')
+        
+        self._connection.commit()
+        cursor.close()
