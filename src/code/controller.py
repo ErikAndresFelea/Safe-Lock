a2ba60ff88b50b3976f5d0592d9ef8ad3532bda0
Erik Andres F.H. <erikandresfelea@gmail.com>
2024-07-02 21:50:45 +0200

    TODO

--- src/code/controller.py
+++ src/code/controller.py
@@ -1,66 +1,87 @@
+import sqlite3 as sql
 from code.login import Login
 from code.register import Register
 from code.passwordManager import PasswordManager
 from code.dataHandler import DataHandler
 
-Operation = bool
-Error = bool
-Msg = str | None
-
 class Controller:
-    def __init__(self, data_handler: DataHandler):
+    def __init__(self, connection: sql.Connection):
         super().__init__()
-        self.data_handler = data_handler
+        self.connection = connection
+        self.authenticated = False
+        self._user_id: str = None
+        self._data_handler: DataHandler = None
+        self._password_manager: PasswordManager = None
+        
 
-    def login(self, name: str, password: str, remember: bool) -> tuple[Error, Operation, Msg]:
-        user_login = Login(name, password, self.data_handler, remember)
-        error, status, data_handler, data = user_login.check_credentials()
+    def login(self, username: str, password: str, remember: bool) -> bool:
+        login = Login(self.connection, username, password, remember)
+        if login.authenticated:
+            self._user_id = login.user_id
+            self._data_handler = login.data_handler
+            self.authenticated = login.authenticated
 
-        # Create an PasswordManager using the dataHandler and the user
-        self.password_manager = PasswordManager(data_handler, data)
-        '''
-        self.password_manager.set_dataHandler(data_handler)
-        self.password_manager.set_username(name)
-        '''
-        return error, status, data
-
-    def register(self, name: str, email: str, password: str) -> tuple[Error, Operation, Msg]:
-        new_user = Register(name, email, password, self.data_handler)
-        return new_user.create_account()
+        self._password_manager = PasswordManager(self._data_handler, self._user_id)
     
-    def forgot_password(self, email: str) -> tuple[Error, Operation, Msg | list[str]]:
-        error, status, data = self.data_handler.recover_password(email)
-        if not status or error:
-            return error, status, data
-        return False, True, data
     
-    def get_last_user(self) -> tuple[Error, Operation, Msg | tuple[str, str]]:
-        error, status, data = self.data_handler.get_last_user()
-        if not status or error:
-            return error, status, data
-        return False, True, data
+    def register(self, username: str, email: str, password: str):
+        pass
+
+    def forgot_password(self, email: str):
+        pass
     
-    ''' Not used yet
-    def get_password(self, id: str) -> tuple[bool, list[str] | None]:
-        confirm, password = self.password_manager.get_password(id)
-        return confirm, password
-    '''
+    def get_last_user(self):
+        pass
 
-    def get_all_passwords(self) -> tuple[Error, Operation, list[list[str]] | Msg]:
-        error, status, data = self.password_manager.get_all_passwords()
-        if not status or error:
-            return error, status, data
-        all_passwords = []
-        for password in data:
-            all_passwords.append(password.get_all())
-        return False, True, all_passwords
+    def get_all_passwords(self):
+        pass
 
-    def add_password(self, data: list[str]) -> tuple[Error, Operation, Msg]:
-        return self.password_manager.add_password(data)
+    def add_password(self, data: list[str]):
+        pass
 
-    def update_password(self, data: list[str]) -> tuple[Error, Operation, Msg]:
-        return self.password_manager.update_password(data)
+    def update_password(self, data: list[str]):
+        pass
 
-    def delete_password(self, id: list[str]) -> tuple[Error, Operation, Msg]:
-        return self.password_manager.delete_password(id)
+    def delete_password(self, id: list[str]):
+        pass
+
+#    def register(self, username: str, email: str, password: str) -> tuple[Error, Operation, Msg]:
+#        new_user = Register(username, email, password, self.data_handler)
+#        return new_user.create_account()
+#    
+#    def forgot_password(self, email: str) -> tuple[Error, Operation, Msg | list[str]]:
+#        error, status, data = self.data_handler.recover_password(email)
+#        if not status or error:
+#            return error, status, data
+#        return False, True, data
+#    
+#    def get_last_user(self) -> tuple[Error, Operation, Msg | tuple[str, str]]:
+#        error, status, data = self.data_handler.get_last_user()
+#        if not status or error:
+#            return error, status, data
+#        return False, True, data
+#    
+#    ''' Not used yet
+#    def get_password(self, id: str) -> tuple[bool, list[str] | None]:
+#        confirm, password = self.password_manager.get_password(id)
+#        return confirm, password
+#    '''
+#
+#    def get_all_passwords(self) -> tuple[Error, Operation, list[list[str]] | Msg]:
+#        error, status, data = self.password_manager.get_all_passwords()
+#        if not status or error:
+#            return error, status, data
+#        all_passwords = []
+#        for password in data:
+#            all_passwords.append(password.get_all())
+#        return False, True, all_passwords
+#
+#    def add_password(self, data: list[str]) -> tuple[Error, Operation, Msg]:
+#        return self.password_manager.add_password(data)
+#
+#    def update_password(self, data: list[str]) -> tuple[Error, Operation, Msg]:
+#        return self.password_manager.update_password(data)
+#
+#    def delete_password(self, id: list[str]) -> tuple[Error, Operation, Msg]:
+#        return self.password_manager.delete_password(id)
     
--- src/code/dataHandler.py
+++ src/code/dataHandler.py
@@ -1,156 +1,126 @@
-import keyring, json
 from cryptography.fernet import Fernet
 
-Error = bool
-Operation = bool
-Msg = str | None
 
 class DataHandler:
-    def __init__(self, token: str | None, file: str):
-        self.key = token
-        self.file = file
-
-
-    # Setter
-    def set_key(self, token: str):
-        self.key = token
+    def __init__(self, token: str):
+        self._key = token
+        self.operation = False
 
 
     ''' Encrypt a string using user key '''
-    def encrypt(self, data: str) -> tuple[Error, Operation, Msg]:
+    def encrypt(self, data: str) -> str:
         try:
             data_to_bytes = data.encode('utf-8')
-            fernet = Fernet(self.key)
+            fernet = Fernet(self._key)
             token = fernet.encrypt(data_to_bytes)
-            token_to_string = token.decode('utf-8')
-            return False, True, token_to_string
+            data = token.decode('utf-8')
+            self.operation = True
         except Exception:
-            msg = "Error al encriptar"
-            return True, False, msg
+            self.operation = False
+        finally:
+            return data
 
 
     ''' Decrypt a string using user key '''
-    def decrypt(self, data: str) -> tuple[Error, Operation, Msg]:
+    def decrypt(self, data: str) -> str:
         try:
             data_to_bytes = data.encode('utf-8')
-            fernet = Fernet(self.key)
+            fernet = Fernet(self._key)
             token = fernet.decrypt(data_to_bytes)
-            token_to_string = token.decode('utf-8')
-            return False, True, token_to_string
+            data = token.decode('utf-8')
+            self.operation = True
         except Exception:
-            msg = "Error al desencriptar"
-            return True, False, msg
+            self.operation = False
+        finally:
+            return data
 
 
-    ''' 
-    Atempts to retriev user credentials from WCM
-    to veryfie if it exists
-    Returns: operation success or a error msg
-    '''
-    def user_exists(self, name: str) -> tuple[Error, Operation, Msg]:
-        try:
-            service_name = "safe_lock"
-            key = keyring.get_password(service_name, name)
-            return False, key is not None, None
-        except Exception as e:
-            print(e.__traceback__)
-            msg = "Error al comprobar disponibilidad del usuario"
-            return True, False, msg
-        
+    def get_last_user(self):
+        pass
 
-    '''
-    Retrieves las user credentials if they enabled the option
-    '''
-    def get_last_user(self) -> tuple[Error, Operation, Msg | tuple[str, str]]:
-        error, status, json_data = self.json_load()
-        if not status or error:
-            return error, status, json_data
-        
-        remember = json_data.get("remember")
-        is_user = False if remember == "" else True
-        if not is_user:
-            return False, False, None
+    def recover_password(self, email: str):
+        pass
 
-        error, status, key = self.obtain_key(remember)
-        if not status:
-            return error, status, None, key
+    def save_key(self, name: str, key: str):
+        pass
 
-        self.set_key(key)
-        encyoted_password = json_data.get('users').get(remember).get('app_password')
-        error, status, password = self.decrypt(encyoted_password)
-        if error or not status:
-            return error, status, password
-        return False, True, [remember, password]
+    def json_load(self):
+        pass
+
+    def json_dump(self, json_data: str):
+        pass
+
+    def user_exists(self, name: str):
+        pass
+
+    def set_key(self, token: str):
+        pass
     
-
-    '''
-    Retrieves users asociated to an email
-    '''
-    def recover_password(self, email: str) -> tuple[Error, Operation, Msg | list[str]]:
-        error, status, json_data = self.json_load()
-        if not status or error:
-                return error, status, json_data
-        data = json_data.get('users', {})
-        users = data.keys()
-        
-        accounts = []
-        for user in users:
-            json_email = data.get(user).get('email')
-            if email == json_email:
-                 accounts.append(user)
-        return False, True, accounts 
-
-
-    '''
-    Stores password in WCM or returns an error
-    '''
-    def save_key(self, name: str, key: str) -> tuple[Error, Operation, Msg]:
-        try:
-            service_name = "safe_lock"
-            keyring.set_password(service_name, name, key)
-            return False, True, None
-        except Exception as e:
-            print(e.__traceback__)
-            msg = "Error al crear el usuario usuario"
-            return True, False, msg
-        
+#    ''' Retrieves las user credentials if they enabled the option '''
+#    def get_last_user(self) -> tuple[Error, Operation, Msg | tuple[str, str]]:
+#        error, status, json_data = self.json_load()
+#        if not status or error:
+#            return error, status, json_data
+#        
+#        remember = json_data.get("remember")
+#        is_user = False if remember == "" else True
+#        if not is_user:
+#            return False, False, None
+#
+#        error, status, key = self.obtain_key(remember)
+#        if not status:
+#            return error, status, None, key
+#
+#        self.set_key(key)
+#        encyoted_password = json_data.get('users').get(remember).get('app_password')
+#        error, status, password = self.decrypt(encyoted_password)
+#        if error or not status:
+#            return error, status, password
+#        return False, True, [remember, password]
+#    
+#    ''' Retrieves users asociated to an email '''
+#    def recover_password(self, email: str) -> tuple[Error, Operation, Msg | list[str]]:
+#        error, status, json_data = self.json_load()
+#        if not status or error:
+#                return error, status, json_data
+#        data = json_data.get('users', {})
+#        users = data.keys()
+#        
+#        accounts = []
+#        for user in users:
+#            json_email = data.get(user).get('email')
+#            if email == json_email:
+#                 accounts.append(user)
+#        return False, True, accounts 
+#
+#    ''' Stores password in WCM or returns an error '''
+#    def save_key(self, name: str, key: str) -> tuple[Error, Operation, Msg]:
+#        try:
+#            service_name = "safe_lock"
+#            keyring.set_password(service_name, name, key)
+#            return False, True, None
+#        except Exception as e:
+#            print(e.__traceback__)
+#            msg = "Error al crear el usuario usuario"
+#            return True, False, msg
+#        
+#    ''' Loads data from a json '''
+#    def json_load(self) -> tuple[Error, Operation, Msg]:
+#        try:
+#            with open(self.file, "r", encoding="utf-8") as json_file:
+#                json_data = json.load(json_file)
+#        except FileNotFoundError:
+#                msg = "No se ha encontrado el archivo"
+#                return True, False, msg
+#        return False, True, json_data
+#
+#    ''' Stores data in a json '''
+#    def json_dump(self, json_data: str) -> tuple[Error, Operation, Msg]:
+#        try:
+#            with open(self.file, "w", encoding="utf-8") as json_file:
+#                json.dump(json_data, json_file, indent=4)
+#        except FileNotFoundError:
+#                msg = "No se ha encontrado el archivo"
+#                return True, False, msg
+#        return False, True, None
     
-    '''
-    Retrieves user key from WCM 
-    Returns: operation success and key or a error msg
-    '''
-    def obtain_key(self, user: str) -> tuple[Error, Operation, Msg]:
-        try:
-            service_name = "safe_lock"
-            key = keyring.get_password(service_name, user)
-            return False, key is not None, key
-        except Exception as e:
-            print(e.__traceback__)
-            msg = "Error al obtener llave"
-            return True, False, msg
-
-
-    '''
-    Loads data from a json
-    '''
-    def json_load(self) -> tuple[Error, Operation, Msg]:
-        try:
-            with open(self.file, "r", encoding="utf-8") as json_file:
-                json_data = json.load(json_file)
-        except FileNotFoundError:
-                msg = "No se ha encontrado el archivo"
-                return True, False, msg
-        return False, True, json_data
-
-
-    '''
-    Stores data in a json
-    '''
-    def json_dump(self, json_data: str) -> tuple[Error, Operation, Msg]:
-        try:
-            with open(self.file, "w", encoding="utf-8") as json_file:
-                json.dump(json_data, json_file, indent=4)
-        except FileNotFoundError:
-                msg = "No se ha encontrado el archivo"
-                return True, False, msg
-        return False, True, None
